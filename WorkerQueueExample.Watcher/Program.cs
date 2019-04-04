e77ee9bbf3297e1451e891ff11c01da6c75967ca
Francois Theron <ftheron@outlook.com>
2019-04-04 10:57:27 +0200

    Add project files.

---
+++ WorkerQueueExample.Watcher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.IO;
+
+namespace WorkerQueueExample.Watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher fileWatcher = new FileSystemWatcher(@"C:\LogsAndReports\FileWatcher");
+            fileWatcher.Created += Process;
+            fileWatcher.EnableRaisingEvents = true;
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        public static void Process(object sender, FileSystemEventArgs e)
+        {
+            var fileContent = File.ReadAllBytes(e.FullPath);
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: fileContent);
+                    Console.WriteLine(" [x] Sent {0}", e.FullPath);
+                }
+            }
+        }
+    }
+}
